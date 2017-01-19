07fb3690c37e2d4bc90266f690151048f4a8d315
root <root@localhost.localdomain>
2017-01-19 19:03:33 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//Program to Create a Dictionary using Binary Search Tree
+
+/*
+ * Created by Devashish Katoriya on 19-01-2017
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
